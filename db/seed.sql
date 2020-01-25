8bebd09830c9e0bbdbbc76ff625670866b1ee009
Bryan Jerger <Bjerger1470@gmail.com>
2020-01-25 15:41:39 -0600

    update db with sql files

--- db/seed.sql
+++ db/seed.sql
@@ -1,16 +1,8 @@
-/*
-DROP DATABASE IF EXISTS employees;
 
-CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
-);
-
+/*Populate the Departments */
 INSERT INTO department (name)
 VALUES ("Sales");
 
@@ -23,13 +15,7 @@ VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
 
-CREATE TABLE role (
-  id INT AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+/*Populate the roles valeys */
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1);
@@ -55,14 +41,7 @@ VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 190000, 4);
 
-CREATE TABLE employee (
-  id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id iNT NOT NULL,
-  manager_id VARCHAR(30),
-  PRIMARY KEY(id)
-);
+/*Create base employees */
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Bryan", "Jerger", 1, null);
